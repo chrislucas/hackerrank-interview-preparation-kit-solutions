be6b1ac9feed1fefa291c2a7dfe1662f2038aebe
chrislucas <christoffer.luccas@gmail.com>
2018-11-14 17:52:09 -0200

    estudos

--- kotlin/sorting/FraudulentActivityNotifications/src/solution/Solution.kt
+++ kotlin/sorting/FraudulentActivityNotifications/src/solution/Solution.kt
@@ -6,6 +6,27 @@ package solution
 
 fun readInt(del: String): Array<Int> = readLine()!!.split(del).mapTo(arrayListOf()) { it -> it.toInt() }.toTypedArray()
 
-fun main(args: Array<String>) {
+fun activityNotifications(expenditure: Array<Int>, d: Int): Int {
+    val limit = expenditure.size-d
+    var acc = 0
+    for(i in 0 until limit) {
+        val r = d + i - 1
+        val copy = expenditure.copyOf()
+        copy.sort(i, r+1)
+        val m = (r - i) / 2 + i
+        val v= if( (d and 1) == 0) {
+            ((copy[m] + copy[m+1]) * 1.0f / 2.0f).toDouble()
+        } else {
+            copy[m].toDouble()
+        }
+        if (v * 2.0f <= copy[i+d])
+            acc++
+    }
+    return acc
+}
 
+fun main(args: Array<String>) {
+    val n = readInt(" ")
+    val values = readInt(" ")
+    println(activityNotifications(values, n[1]))
 }
